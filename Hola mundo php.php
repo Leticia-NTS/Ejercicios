e298ce568d7e008a96ad345846b0cd6a1b1696f7
Leticia-NTSe <letisia.saravia@gmail.com>
2024-11-26 08:33:08 -0600

    Nada.

--- Hola mundo php.php
+++ Hola mundo php.php
@@ -1,12 +1,11 @@
 <html>
->
 
 <head>
-    <title>Prueba de php</title>
+    <title>Prueba de PHP</title>
 </head>
 
 <body>
-    <?php echo '<p>Hola mundo</p>;' ?>
+    <?php echo '<p>Hola mundo</p>'; ?>
 </body>
 
 </html>
